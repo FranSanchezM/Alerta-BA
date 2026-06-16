f36d6d43dd80040ef8b2e48665e84028d65a1154
Francis <joafransanmer@gmail.com>
2026-06-16 19:53:00 -0300

    fix: remove stale src/ template directory

--- src/components/container.tsx
+++
@@ -1,8 +0,0 @@
-import type { PropsWithClassName } from "@/types";
-import type { PropsWithChildren } from "react";
-
-interface Props extends PropsWithChildren, PropsWithClassName {}
-
-export function Container({ children, className }: Props) {
-	return <div className="flex flex-1 flex-col">{children}</div>;
-}
--- src/lib/config.ts
+++
@@ -1,3 +0,0 @@
-const {} = process.env;
-
-export const config = {};
